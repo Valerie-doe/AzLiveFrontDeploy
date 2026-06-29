6d7ecf0011be6db9d4c279af9d39d8cd091a5629
Andriamihaja <andoandria2386@gmail.com>
2026-06-29 17:12:47 +0300

    front

--- src/shared/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as StatCard } from './components/StatCard';
-export { default as PaginationControls } from './components/PaginationControls';
-export { default as SearchInput } from './components/SearchInput';
-export { default as ConfirmModal } from './components/ConfirmModal';
--- src/shared/layouts/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as Sidebar } from './components/Sidebar';
-export { default as Header } from './components/Header';
-export { default as MobileMenu } from './components/MobileMenu';
-export { default as PromoAlert } from './components/PromoAlert';
